08a1678f29e67651e3e58068ab672a2889f5a05e
hurtaditoo <al415507@uji.es>
2024-11-17 00:42:28 +0100

    Iteration 5 | User Component

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,6 +1,11 @@
 
-function User() {
-
+function User({ name, handle }) {
+    return (
+        <span className="user">
+            <span className="name"> {name} </span>
+            <span className="handle"> @{handle} </span>
+        </span>
+    )
 }
 
 export default User;
