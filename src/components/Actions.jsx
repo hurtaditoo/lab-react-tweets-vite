be41bf85c835282558373e63de71474f57ac31ed
hurtaditoo <al415507@uji.es>
2024-11-17 00:55:12 +0100

    Fix

--- src/components/Actions.jsx
+++ src/components/Actions.jsx
@@ -2,11 +2,11 @@
 function Actions() {
     return (
         <div className="actions">
-        <i class="far fa-comment" data-testid="comment-icon"></i>
-        <i class="fas fa-retweet" data-testid="retweet-icon"></i>
-        <i class="far fa-heart" data-testid="heart-icon"></i>
-        <i class="fas fa-share" data-testid="share-icon"></i>
-</div>
+            <i class="far fa-comment" data-testid="comment-icon"></i>
+            <i class="fas fa-retweet" data-testid="retweet-icon"></i>
+            <i class="far fa-heart" data-testid="heart-icon"></i>
+            <i class="fas fa-share" data-testid="share-icon"></i>
+        </div>
     )
 }
 
